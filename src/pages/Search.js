4c8b21bc7744bc8fe61db7aeb965dacf5d2b7b04
Thamires Lourenço <thami.lourenco@outlook.com>
2022-03-08 18:45:35 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchBtn: true,
+    };
+  }
+
+  btnValidade = (event) => {
+    const { value } = event.target;
+    const inputSize = 2;
+    this.setState(() => ({
+      searchBtn: value.length < inputSize,
+    }));
+  }
+
   render() {
+    const { searchBtn } = this.state;
     return (
       <div data-testid="page-search">
-        Search
+        <label htmlFor="search">
+          Search
+          <input
+            data-testid="search-artist-input"
+            id="search"
+            type="text"
+            name="search"
+            onChange={ this.btnValidade }
+          />
+        </label>
+        <button
+          data-testid="search-artist-button"
+          id="searchBtn"
+          name="searchBtn"
+          type="submit"
+          disabled={ searchBtn }
+        >
+          Pesquisar
+        </button>
         <Header />
       </div>
     );
